dbfb30ce81ca9353cfc4e82ab8917fcc191566f0
David Cui <cuidavid02@gmail.com>
2023-02-11 15:01:50 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from pathlib import Path
-
-
-my_csv = Path(r"C:/Users/davidcui02/Desktop/CDP_Primary_demographics_v1.csv")
-df = pd.read_csv(my_csv.resolve())
-df.head()
-
